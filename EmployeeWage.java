be4c02e7e6cc4acf9c73fcc6018522f962180b6c
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-30 00:38:04 +0530

    UC:5 Calculating Wages For Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -14,6 +14,10 @@
 
 	int Part_Day_Hr = 4;
 
+	int Monthly_Wage = 0;
+
+
+	for(int i=1;i<=20;i++){
 
 	double Emp_Check = Math.floor(Math.random() *10 )%3;
 
@@ -25,32 +29,30 @@
    	switch(Random){  
 
 	//Case statements  
-   
+
 	 case 1:   {
-    			System.out.println("Employee is present");
-			
+
 			int Emp_Wage = Wage_Per_Day * Full_Day_Hr;
-			
-			System.out.println("Employee daily wage is " + Emp_Wage );
+
+			Monthly_Wage+=Emp_Wage;
    		    }
     		    break;  
     	 case 2:  {
-    			System.out.println("Employee is part time");
-			
+
 			int Emp_Wage = Wage_Per_Day * Part_Day_Hr;
-		
-			System.out.println("Employee daily wage is " + Emp_Wage );
+
+			Monthly_Wage+=Emp_Wage;
   		  } 
    		   break;  
   
        //Default case statement  
-         default:   {
-			System.out.println("Employee is absent");
-	    
-			System.out.println("Employee daily wage is 0 " );
-	    	    }  
-    
-		}
-        	
+         default:   
+
+		   break;
+
+			}
+       	       }
+	System.out.println("Monthly Wage Of Employee Is " + Monthly_Wage);
+
 	 }
    }
