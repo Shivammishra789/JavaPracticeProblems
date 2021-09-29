e23bc1055157e9784c3bf54f18514f420e289175
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-29 22:30:58 +0530

    UC:1 Random check Employee Present or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,5 +4,19 @@
 
 	System.out.println("Welcome to Employee Wage Computation Program !");
         
+	
+	int Is_Present = 1;
+
+	double Emp_Check = Math.floor(Math.random() *10 )%2;
+
+	if (Emp_Check == Is_Present ) 
+
+
+		System.out.println("Employee is present");
+
+	else 
+
+		System.out.println("Employee is absent");
+
         }
     }
