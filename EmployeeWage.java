4cd6ca6fc950c69f4e128b92b54afeceedd29964
Shivammishra789 <shivammishra7789@gmail.com>
2021-09-30 00:20:04 +0530

    UC:4 Switch Case Statement Used

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,7 +7,7 @@
 	int Wage_Per_Day = 20;
 
 	int Full_Day_Hr = 8;
-	
+
 	int Is_Part_Time = 2;
 
 	int Is_Present = 1;
@@ -17,32 +17,40 @@
 
 	double Emp_Check = Math.floor(Math.random() *10 )%3;
 
-	if (Emp_Check == Is_Present ) {
+	int Random = (int) Emp_Check;
 
-		System.out.println("Employee is present");
 
-		int Emp_Wage = Wage_Per_Day * Full_Day_Hr;
+	//Switch expression  
 
-		System.out.println("Employee daily wage is " + Emp_Wage );
+   	switch(Random){  
 
+	//Case statements  
+   
+	 case 1:   {
+    			System.out.println("Employee is present");
+			
+			int Emp_Wage = Wage_Per_Day * Full_Day_Hr;
+			
+			System.out.println("Employee daily wage is " + Emp_Wage );
+   		    }
+    		    break;  
+    	 case 2:  {
+    			System.out.println("Employee is part time");
+			
+			int Emp_Wage = Wage_Per_Day * Part_Day_Hr;
+		
+			System.out.println("Employee daily wage is " + Emp_Wage );
+  		  } 
+   		   break;  
+  
+       //Default case statement  
+         default:   {
+			System.out.println("Employee is absent");
+	    
+			System.out.println("Employee daily wage is 0 " );
+	    	    }  
+    
 		}
-
-	else if (Emp_Check == Is_Part_Time) {
-
-		System.out.println("Employee is part time");
-
-		int Emp_Wage = Wage_Per_Day * Part_Day_Hr;
-
-		System.out.println("Employee daily wage is " + Emp_Wage );
-
-		}
-
-	else {
-
-		System.out.println("Employee is absent");
-
-		System.out.println("Employee daily wage is 0 " );
-
-		}
-        }
-    }
+        	
+	 }
+   }
